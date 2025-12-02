0f14286e8bd327b66ab39e7b52930c8b1f953de0
Alex <77193485+StonickAlex@users.noreply.github.com>
2025-12-02 03:11:47 +0100

    quiz_alex

--- server.js
+++ server.js
@@ -9,22 +9,15 @@ app.use(express.static(path.join(__dirname)));
 
 // Главная страница - quiz.html
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'quiz.html'));
+    res.sendFile(path.join(__dirname, 'quiz_alex.html'));
 });
 
 // Fallback для всех остальных маршрутов
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'quiz.html'));
+    res.sendFile(path.join(__dirname, 'quiz_alex.html'));
 });
 
 app.listen(PORT, () => {
-  console.log(`Сервер запущен на порту ${PORT}`);
-  console.log(`Откройте http://localhost:${PORT} в браузере`);
+    console.log(`Сервер запущен на порту ${PORT}`);
+    console.log(`Откройте http://localhost:${PORT} в браузере`);
 });
-
-
-
-
-
-
-
